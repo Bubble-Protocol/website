80d3386d96689baacc955b2a4fce796b2d00926e
Dave.Potter <dave@datonalabs.org>
2023-04-05 22:09:35 +0100

    initial version for test and review

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+function initialiseFAQExpanders() {
+  var faqs = document.getElementsByClassName("faq-button");
+  for (let i=0; i<faqs.length; i++) {
+    faqs[i].addEventListener('click', function() {
+      this.classList.toggle("expanded");
+      const content = this.nextElementSibling.getElementsByClassName("faq-text")[0];
+      content.classList.toggle("hidden");
+    });    
+  }
+}
