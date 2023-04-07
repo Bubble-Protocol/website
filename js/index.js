24c1702032c51462fa60c2830c8c4a882955a69b
Dave.Potter <dave@datonalabs.org>
2023-04-07 10:29:07 +0100

    move logo to header and change hero titles

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 function initialiseFAQExpanders() {
   var faqs = document.getElementsByClassName("faq-button");
   for (let i=0; i<faqs.length; i++) {
-    faqs[i].addEventListener('click', function() {
+    faqs[i].addEventListener("click", function() {
       this.classList.toggle("expanded");
       const content = this.nextElementSibling.getElementsByClassName("faq-text")[0];
       content.classList.toggle("hidden");
