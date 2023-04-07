d9501252281b5c4c2e622c0a53cba4e13ca93ca0
Dave.Potter <dave@datonalabs.org>
2023-04-07 10:38:35 +0100

    shrink header on scroll

--- js/index.js
+++ js/index.js
@@ -2,10 +2,21 @@
 function initialiseFAQExpanders() {
   var faqs = document.getElementsByClassName("faq-button");
   for (let i=0; i<faqs.length; i++) {
-    faqs[i].addEventListener("click", function() {
+    faqs[i].addEventListener('click', function() {
       this.classList.toggle("expanded");
       const content = this.nextElementSibling.getElementsByClassName("faq-text")[0];
       content.classList.toggle("hidden");
     });    
+  }
+}
+
+function initialise() {
+  initialiseFAQExpanders();
+  window.onscroll = function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("header").classList.add('header-scroll');
+    } else {
+      document.getElementById("header").classList.remove('header-scroll');
+    }
   }
 }
