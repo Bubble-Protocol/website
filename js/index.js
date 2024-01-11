46d8ea3670ee524b0d85eca831205e107c14460b
Dave.Potter <dave@datonalabs.org>
2024-01-11 19:46:59 +0000

    skeleton homepage with header and footer

--- js/index.js
+++ js/index.js
@@ -7,30 +7,6 @@ function initialise() {
   initialiseFAQExpanders();
 }
 
-
-function initialiseFAQExpanders() {
-  var faqs = document.getElementsByClassName("faq-button");
-  for (let i=0; i<faqs.length; i++) {
-    faqs[i].addEventListener('click', function() {
-      this.classList.toggle("expanded");
-      const content = this.nextElementSibling.getElementsByClassName("faq-text")[0];
-      content.classList.toggle("hidden");
-    });    
-  }
-}
-
-
-function initialiseHeader() {
-  window.onscroll = function() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").classList.add('header-scroll');
-    } else {
-      document.getElementById("header").classList.remove('header-scroll');
-    }
-  }
-}
-
-
 function joinEmailList() {
   grecaptcha.ready(function() {
     grecaptcha.execute('6LeSTW0lAAAAAPHSOMbGylNsXD7evuMBMSRacUsC', {action: 'submit'})
