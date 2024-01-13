c348a1e12dd3bdbcd84e900e19f3d55bfdea1bf2
Dave.Potter <dave@datonalabs.org>
2024-01-13 12:09:28 +0000

    animate on scroll into view

--- js/index.js
+++ js/index.js
@@ -1,55 +1,27 @@
 
-const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+$(function() {
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html");
+  const page = document.getElementById('page');
+  page.addEventListener("scroll", animate);
+})
 
 
-function initialise() {
-  initialiseHeader();
-  initialiseFAQExpanders();
-}
+/**
+ * Animate on scroll into view
+ */
 
-function joinEmailList() {
-  grecaptcha.ready(function() {
-    grecaptcha.execute('6LeSTW0lAAAAAPHSOMbGylNsXD7evuMBMSRacUsC', {action: 'submit'})
-      .then(function(token) {
-        const email = $("#email").val();
-        return fetch("https://vault.bubbleprotocol.com:8125/bubble-join-email-list?email="+email+"&token="+token);
-      })
-      .then(() => {
-        $("#email").text('');
-        toggleHidden("#email", "#join-button", "#recaptcha-notice", "#thankyou-for-joining-text");
-      })
-      .catch(err => {
-        console.error(err);
-        toggleHidden("#email", "#join-button", "#recaptcha-notice", "#something-went-wrong-text");
-      });
-  });
-}
-window.joinEmailList = joinEmailList;
+function animate() {
+  var reveals = document.querySelectorAll(".animate");
 
-
-function validateEmail(event) {
-  const email = $("#email").val();
-  if (EMAIL_REGEX.test(email)) {
-    enable("#join-button");
-    if (event.key === 'Enter') joinEmailList();
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 0;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("animation-active");
+    } else {
+      reveals[i].classList.remove("animation-active");
+    }
   }
-  else disable("#join-button");
-}
-window.validateEmail = validateEmail;
-
-
-//
-// CSS functions
-//
-
-function toggleHidden(...ids) {
-  ids.forEach(id => $(id).toggleClass('hidden'));
-}
-
-function disable(...ids) {
-  ids.forEach(id => $(id).addClass('disabled'));
-}
-
-function enable(...ids) {
-  ids.forEach(id => $(id).removeClass('disabled'));
 }
